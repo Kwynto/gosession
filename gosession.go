4b4545b6a9cba242db2c443848f39c432121de5b
Constantine Zavezeon (Kwynto) <kwynto@mail.ru>
2022-06-05 22:25:48 +0300

    Removing functions and small edit

--- gosession.go
+++ gosession.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 )
 
-const COOKIE_NAME = "sessionId"
+const (
+	GOSESSION_COOKIE_NAME string = "sessionId"
+	GOSESSION_MAX_AGE     int    = 43_200 // Max age is 12 hours.
+)
 
 type sessionId string
 type Session map[string]interface{}
 type Sessions map[sessionId]Session
 
+// TODO: Сделать очистку сервеного хранилища сессий от старых записей
 var AllSessions Sessions
 
 // Privat
@@ -18,11 +22,40 @@ var AllSessions Sessions
 func generateId() sessionId {
 	b := make([]byte, 32)
 	rand.Read(b)
-	// return fmt.Sprintf("%x", b)
 	return sessionId(b)
 }
 
+func getOrSetCookie(w *http.ResponseWriter, r *http.Request) sessionId {
+	data, err := r.Cookie(GOSESSION_COOKIE_NAME)
+	if err != nil {
+		id := generateId()
+		cookie := &http.Cookie{
+			Name:   GOSESSION_COOKIE_NAME,
+			Value:  string(id),
+			MaxAge: 0,
+		}
+		http.SetCookie(*w, cookie)
+		return id
+	}
+	return sessionId(data.Value)
+}
+
+func deleteCookie(w *http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:   GOSESSION_COOKIE_NAME,
+		Value:  "",
+		MaxAge: -1,
+	}
+	http.SetCookie(*w, cookie)
+}
+
 // Public
+
+func (id sessionId) Set(name string, value interface{}) {
+	ses := AllSessions[id]
+	ses[name] = value
+	AllSessions[id] = ses
+}
 
 func (id sessionId) GetAll() Session {
 	return AllSessions[id]
@@ -33,30 +66,20 @@ func (id sessionId) GetOne(name string) interface{} {
 	return data[name]
 }
 
-func (id sessionId) Set(name string, value interface{}) {
-	ses := id.GetAll()
-	ses[name] = value
-	AllSessions[id] = ses
+func (id sessionId) RemoveSession(w *http.ResponseWriter) {
+	delete(AllSessions, id)
+	deleteCookie(w)
 }
 
-func getOrSetId(w *http.ResponseWriter, r *http.Request) sessionId {
-	data, err := r.Cookie(COOKIE_NAME)
-	if err != nil {
-		gi := generateId()
-		cookie := &http.Cookie{
-			Name:   COOKIE_NAME,
-			Value:  string(gi),
-			MaxAge: 0,
-		}
-		http.SetCookie(*w, cookie)
-		return gi
-	}
-	return sessionId(data.Value)
+func (id sessionId) RemoveValue(name string) {
+	data := AllSessions[id]
+	delete(data, name)
+	AllSessions[id] = data
 }
 
 func Start(w *http.ResponseWriter, r *http.Request) sessionId {
-	id := getOrSetId(w, r)
-	data := id.GetAll()
+	id := getOrSetCookie(w, r)
+	data := AllSessions[id]
 	if data == nil {
 		data := make(Session, 0)
 		AllSessions[id] = data
